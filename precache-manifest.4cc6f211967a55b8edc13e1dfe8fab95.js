663310fa5c4d45b655a54aa8689dd8547fe1f770
fika_nofa <lalaina.ramarivelo@gmail.com>
2020-10-09 00:03:13 -0500

    Updates

---
+++ precache-manifest.4cc6f211967a55b8edc13e1dfe8fab95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0ec10896f58dc86ee797b7e141a9c6",
+    "url": "/kialala.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/kialala.github.io/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "79e1c3186c852c3fbdcd",
+    "url": "/kialala.github.io/robofriends/static/css/main.211f6fba.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/kialala.github.io/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kialala.github.io/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e1c3186c852c3fbdcd",
+    "url": "/kialala.github.io/robofriends/static/js/main.530ad8d8.chunk.js"
+  },
+  {
+    "revision": "886a768aa46576ba92f1",
+    "url": "/kialala.github.io/robofriends/static/js/runtime-main.66418f76.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kialala.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
